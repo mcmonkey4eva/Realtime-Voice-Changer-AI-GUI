152afcad05d59739d2b29c2146e2de1dcc9aa3f0
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2026-09-06 23:14:21 -0700

    fix the launch scripts

--- go-realtime_gui.bat
+++ go-realtime_gui.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 set "SCRIPT_DIR=%~dp0"
 set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
 cd /d "%SCRIPT_DIR%"
-set "PATH=%SCRIPT_DIR%\runtime;%PATH%"
-echo 首次启动程序可能需要耐心等待20秒
-runtime\python.exe -I realtime_gui.py
+call "%SCRIPT_DIR%\.venv\Scripts\activate.bat"
+echo Launching the program for the first time may require waiting up to 20 seconds.
+python realtime_gui.py
 pause
--- go-webui.bat
+++ go-webui.bat
@@ -3,9 +3,9 @@ chcp 65001 >nul
 set "SCRIPT_DIR=%~dp0"
 set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
 cd /d "%SCRIPT_DIR%"
-set "PATH=%SCRIPT_DIR%\runtime;%PATH%"
+call "%SCRIPT_DIR%\.venv\Scripts\activate.bat"
 set "GRADIO_ANALYTICS_ENABLED=False"
 set "NO_PROXY=localhost,127.0.0.1,::1,%NO_PROXY%"
-echo 首次启动程序可能需要耐心等待20秒
+echo Launching the program for the first time may require waiting up to 20 seconds.
 runtime\python.exe -I webui.py --pycmd runtime\python.exe --port 7897
 pause
